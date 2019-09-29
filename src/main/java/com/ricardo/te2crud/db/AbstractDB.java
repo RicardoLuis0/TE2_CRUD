489af178b4ce53ec7ac15b1dcea266d79b8afdd2
RicardoLuis0 <ricolvs123@gmail.com>
2019-09-29 19:02:28 -0300

    abstract database class

---
+++ src/main/java/com/ricardo/te2crud/db/AbstractDB.java
@@ -0,0 +1,14 @@
+package com.ricardo.te2crud.db;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ricardo.te2crud.db.data.DBProduct;
+
+public abstract class AbstractDB {
+	public abstract List<DBProduct> searchProducts(String query) throws SQLException;
+	public abstract DBProduct addProduct(DBProduct prod) throws SQLException;//id is ignored
+	public abstract DBProduct getProduct(int id) throws SQLException;
+	public abstract DBProduct updateProduct(DBProduct prod) throws SQLException;//id is used to find item in DB
+	public abstract DBProduct removeProduct(int id) throws SQLException;
+}
