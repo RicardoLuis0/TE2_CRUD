f11ef6eb8188e6e3ac6369769ed24b16ee588d4d
RicardoLuis0 <ricolvs123@gmail.com>
2019-09-29 19:32:10 -0300

    add sql

---
+++ sql/generate.sql
@@ -0,0 +1,12 @@
+drop database TE2Crud;
+
+create database TE2Crud;
+
+create table Products (
+	id int not null,
+	name varchar(128) not null,
+	img_url varchar(128) not null,
+	short_description varchar(256) not null,
+	long_description varchar(2048) not null,
+	constraint pk_products primary key (id)
+);
