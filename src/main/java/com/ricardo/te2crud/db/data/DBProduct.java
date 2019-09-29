68a844734254574a8ea975fa62adf0e4ddf360ed
RicardoLuis0 <ricolvs123@gmail.com>
2019-09-29 19:32:22 -0300

    update DBProduct to reflect SQL

--- src/main/java/com/ricardo/te2crud/db/data/DBProduct.java
+++ src/main/java/com/ricardo/te2crud/db/data/DBProduct.java
@@ -3,18 +3,20 @@ package com.ricardo.te2crud.db.data;
 import com.ricardo.te2crud.jdbc.JDBCConnector.RSGetter;
 
 public class DBProduct {
-	public static final RSGetter<DBProduct> getter=(r)->new DBProduct(r.getInt(1),r.getString(2),r.getString(3),r.getString(4));
+	public static final RSGetter<DBProduct> getter=(r)->new DBProduct(r.getInt(1),r.getString(2),r.getString(3),r.getString(4),r.getString(5));
 	private int id;
 	private String name;
 	private String img_url;
-	private String description;
+	private String shortDescription;
+	private String longDescription;
 	public DBProduct() {
 	}
-	public DBProduct(int id, String name, String img_url, String description) {
+	public DBProduct(int id, String name, String img_url, String shortDescription, String longDescription) {
 		this.id = id;
 		this.name = name;
 		this.img_url = img_url;
-		this.description = description;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
 	}
 	public int getId() {
 		return id;
@@ -34,10 +36,16 @@ public class DBProduct {
 	public void setImg_url(String img_url) {
 		this.img_url = img_url;
 	}
-	public String getDescription() {
-		return description;
+	public String getShortDescription() {
+		return shortDescription;
 	}
-	public void setDescription(String description) {
-		this.description = description;
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	public String getLongDescription() {
+		return longDescription;
+	}
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
 }
