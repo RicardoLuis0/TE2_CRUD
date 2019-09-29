126cc101bee5085d9e4f1d6121a8f6d543f3bd6f
RicardoLuis0 <ricolvs123@gmail.com>
2019-09-29 19:35:52 -0300

    fix sql

--- sql/generate.sql
+++ sql/generate.sql
@@ -5,7 +5,7 @@ create database TE2Crud;
 use TE2Crud;
 
 create table Products (
-	id int not null,
+	id int not null auto_increment,
 	name varchar(128) not null,
 	img_url varchar(128) not null,
 	short_description varchar(256) not null,
